5b3a573e4b1623060a168d48508e51badea623e4
Marcelo Salazar Najar <47803149+marcelosdata@users.noreply.github.com>
2022-03-18 16:46:20 +0000

    Created 2 ways to doing it

---
+++ index.js
@@ -0,0 +1,12 @@
+//Convert text to string
+//Description:
+//We need a function that can transform a number into a string.
+
+//What ways of achieving this do you know?
+
+let transformToNumber = num => num + '';
+
+console.log(typeof(transformToNumber(2)))
+
+let secondTransform = num => toString(num);
+console.log(typeof(secondTransform(10)))
