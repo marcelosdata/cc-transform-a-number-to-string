c8d193c802b0c67573271c054ccd786a6e15f30a
Marcelo Salazar Najar <47803149+marcelosdata@users.noreply.github.com>
2022-03-29 02:44:48 +0000

    added counting sheeps array challenge

--- index.js
+++ index.js
@@ -10,3 +10,26 @@ console.log(typeof(transformToNumber(2)))
 
 let secondTransform = num => toString(num);
 console.log(typeof(secondTransform(10)))
+
+
+//adding a nre problem
+  /* 
+Description:
+If you can't sleep, just count sheep!!
+
+Task:
+Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+
+  */
+
+var countSheep = function(num){
+  let contador = ''
+  for(let i = 1 ; i<=num ; i++) { //se iteran las cantidades donde este sea n veces menor que num
+     contador += `${i} sheep...`  //aca se agrega a la variable contador cada resultado con un template literal
+  }
+  return contador //se retorna el resultado aca fuera del loop que envie el resultado en el loop hacia la variable contador
+}
+//explain it 
+
+console.log(countSheep(3))
