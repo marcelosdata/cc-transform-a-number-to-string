1cb539a0c376cee6a3f5effe144b9a028a6a5446
Marcelo Salazar Najar <47803149+marcelosdata@users.noreply.github.com>
2022-04-07 15:35:14 +0000

    added stringy chalenge

--- index.js
+++ index.js
@@ -248,3 +248,24 @@ console.log(digitize(4432))
 function digitize2(n) {
   return String(n).split('').map(Number).reverse()
 }
+
+/*
+write me a function stringy that takes a size and returns a string of alternating '1s' and '0s'.
+
+the string should start with a 1.
+
+a string with size 6 should return :'101010'.
+
+with size 4 should return : '1010'.
+
+with size 12 should return : '101010101010'.
+
+The size will always be positive and will only use whole numbers.*/
+
+let stringy = (size) => {
+ let str = ''
+   return str.padStart(size.length,'10')  
+}
+
+console.log(stringy('size'))
+  
